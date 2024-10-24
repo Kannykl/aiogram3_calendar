a5f38c648a7f06c17c10f490b0b30599a6fd90fa
Kannykl <oleg.akulinushkin.01@mail.ru>
2024-10-24 23:49:01 +0300

    feature: Русский язык по умолчанию

--- aiogram_calendar/schemas.py
+++ aiogram_calendar/schemas.py
@@ -49,8 +49,8 @@ class CalendarLabels(BaseModel):
     months: conlist(str, max_length=12, min_length=12) = [
         "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
     ]
-    cancel_caption: str = Field(default='Cancel', description='Caprion for Cancel button')
-    today_caption: str = Field(default='Today', description='Caprion for Cancel button')
+    cancel_caption: str = Field(default='Отмена', description='Caption for Cancel button')
+    today_caption: str = Field(default='Сегодня', description='Caption for Cancel button')
 
 
 HIGHLIGHT_FORMAT = "[{}]"
